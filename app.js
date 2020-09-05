43d4752d0ea2eb81b281a93ac277e880429c0fb0
Ann Peng <boaimpact@gmail.com>
2020-09-05 20:25:22 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+
+const bodyParser = require('body-parser')
+
 const restaurantList = require('./restaurant.json')
 
 const Restaurant = require('./models/restaurant') //載入Restaurane model
@@ -22,17 +25,33 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 //app.get('/', (req, res) => {
 //  res.render('index', { restaurants: restaurantList.results })
 //})
 
+//瀏覽所有餐廳
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
+
+//新增餐廳資料
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  return Restaurant.create({ name })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(console.log(error)))
+})
+
 
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
