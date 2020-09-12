5c040c5aa0cdd350b32929e2b003ca6c63566c76
Ann Peng <boaimpact@gmail.com>
2020-09-12 16:30:34 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const port = 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
+
 const app = express()
-
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,14 +1,9 @@
-const mongoose = require('mongoose')
+
 const Restaurant = require('../restaurant')
 const restaurantData = require('./restaurant.json')
 
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
   console.log('mongodb connected!')
